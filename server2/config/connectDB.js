87266008fceb57e1f7fcebf68e4bf19e56fc7f77
BaggyBro <anuraagbagal@gmail.com>
2024-07-28 09:56:25 +0530

    changes

--- server2/config/connectDB.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-require('dotenv').config()
-
-const sequelize = new Sequelize('trial', process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = sequelize
